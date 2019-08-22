b48c61603c030bd64c9ae684aade8d025dc63aa6
li.xiaolei <lixiaolei327@gmail.com>
2019-08-22 17:30:43 +0800

    header

---
+++ cnode-test/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <img src="" alt="">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
