bb0b49c4e593d804902ecd50d16a848434d37148
li.xiaolei <lixiaolei327@gmail.com>
2019-08-24 15:17:54 +0800

    postlist

---
+++ cnode-test/src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="loading" v-if="isLoading">
+            <img src="../assets/loading.gif" alt="">
+        </div>
+        <div>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+</template>    
+
+<script>
+export default {
+    name: "PostList",
+    data() {
+        return {
+            isLoading: false
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
